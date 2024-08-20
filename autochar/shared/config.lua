637a60c9d8312397b5bd503a969841e6ec46415d
Arkadiy40000 <94859110+Arkadiy40000@users.noreply.github.com>
2024-08-21 00:57:14 +0300

    Update config.lua

--- autochar/shared/config.lua
+++ autochar/shared/config.lua
@@ -1,20 +1,32 @@
 Config = {}
 
--- Addresses
-Config.Addresses = {
-    {playerPos = vector4(268.9070, -1985.6338, 20.5988, 130.5677), carPos = vector4(285.1616, -1990.6913, 19.9976, 228.2416), name = "Test Road, 1"},
-    {playerPos = vector4(416.5999, -1759.6349, 29.5102, 314.5974), carPos = vector4(399.4098, -1752.6809, 28.7864, 50.4844), name = "Test Road 2"},
-    {playerPos = vector4(-28.4994, -1870.1732, 25.0200, 291.5768), carPos = vector4(-22.5167, -1853.1211, 24.5667, 320.3696), name = "Test Road 3"},
+Config.Names = {
+    Hispanic = {
+        firstNames = {"Juan", "Carlos", "Luis"},
+        lastNames = {"Garcia", "Martinez", "Lopez"},
+        pedModels = {"a_m_m_mexcntry_01", "a_m_m_mexlabor_01"},
+        vehicles = {"tornado", "buccaneer", "manana"}
+    },
+    AfricanAmerican = {
+        firstNames = {"Tyrone", "Malik", "DeShawn"},
+        lastNames = {"Jackson", "Brown", "Davis"},
+        pedModels = {"a_m_m_afriamer_01", "a_m_m_bevhills_01"},
+        vehicles = {"voodoo", "peyote", "primo"}
+    },
+    European = {
+        firstNames = {"John", "Michael", "Edward"},
+        lastNames = {"Smith", "Johnson", "Williams"},
+        pedModels = {"a_m_m_eastsa_01", "a_m_m_eastsa_02"},
+        vehicles = {"infernus", "cheetah", "elegy"}
+    }
 }
 
--- Vehicle Models
-Config.Vehicles = {"emperor", "dilettante", "minivan", "pigalle", "stanier"}
+Config.Addresses = {
+    {playerPos = vector4(268.9070, -1985.6338, 20.5988, 130.5677), carPos = vector4(285.1616, -1990.6913, 19.9976, 228.2416), name = "Test Road, 1", nationality = "Hispanic", isBusy = false},
+    {playerPos = vector4(416.5999, -1759.6349, 29.5102, 314.5974), carPos = vector4(399.4098, -1752.6809, 28.7864, 50.4844), name = "Test Road 2", nationality = "AfricanAmerican", isBusy = false},
+    {playerPos = vector4(-28.4994, -1870.1732, 25.0200, 291.5768), carPos = vector4(-22.5167, -1853.1211, 24.5667, 320.3696), name = "Test Road 3", nationality = "European", isBusy = false}
+}
 
--- Ped Models
-Config.Peds = {"a_m_m_soucent_01", "a_m_m_soucent_02", "a_m_m_soucent_03", "a_m_y_soucent_01", "a_m_y_soucent_02", "a_m_y_soucent_03"}
 
--- Names
-Config.Names = {"John", "Michael", "David", "Chris", "James"}
 
--- Surnames
-Config.Surnames = {"Smith", "Johnson", "Brown", "Williams", "Jones"}
+
